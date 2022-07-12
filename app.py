f58619d836bf50b98674b839da901e87bec96d9a
laksh <lakshyam38@gmail.com>
2022-07-12 19:01:18 +0530

    train with hyperparameters

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+from model import linear_regressor
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route("/train", methods=["GET", "POST"])
+def train_model():
+    if request.method == 'GET':
+        return render_template('train.html')
+    else:
+        data = dict(request.form)
+        fit_intercept = True if data["fi"] == "true" else False 
+        normalize = True if data["n"] == "true" else False
+
+        linear_regressor(FI=fit_intercept, n=normalize)
+
+        return render_template('train.html', done_text="Trained successfully!")
+
 @app.route('/predict',methods=['POST'])
 def predict():
+    model = pickle.load(open('model.pkl', 'rb'))
 
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
@@ -22,7 +37,8 @@ def predict():
 
 @app.route('/results',methods=['POST'])
 def results():
-
+    model = pickle.load(open('model.pkl', 'rb'))
+    
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
